773ce197d92cc2e8ae9fe5c4e6475618bddb5748
Sunil Sadasivan <sunils34@gmail.com>
2013-08-19 18:01:32 -0700

    Initial upload of setup.py and xorcrypt dir

---
+++ setup.py
@@ -0,0 +1,15 @@
+
+from distutils.core import setup
+
+setup(
+    name='xorcrypt',
+    version='0.1dev',
+    packages=['xorcrypt',],
+    license='MIT License',
+    long_description=open('README.md').read(),
+    author="Sunil Sadasivan",
+    author_email="sunil@bufferapp.com.com",
+    maintainer="Sunil Sadasivan",
+    maintainer_email="sunil@bufferapp.com",
+    url="http://github.com/sunils34/python-xorcrypt"
+)
