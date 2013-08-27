deec984ac45eda66634270515139fc2c2f6c7ccf
Sunil Sadasivan <sunils34@gmail.com>
2013-08-27 16:15:34 -0700

    Add rand small test case

--- tests/test_xorcrypt.py
+++ tests/test_xorcrypt.py
@@ -1,11 +1,23 @@
-from xorcrypt.xorcrypt import *
+from xorcrypt.xorcrypt import xor_encrypt
+from xorcrypt.xorcrypt import xor_decrypt
 import unittest
+import random
+import string
 
-class Decryption(unittest.TestCase):
+class XorCryptTestSuite(unittest.TestCase):
+
+    def assert_encrypt_then_decrypt(self, data, key):
+        assert data == xor_decrypt(xor_encrypt(data, key), key)
+
     def test_simple(self):
         key = 'absd34DScx98SewlcCsDwp1297Snmde2'
-        orig = "This is a test string"
-        assert xor_decrypt(xor_encrypt(orig, key), key)
-        return
+        data = "This is a test string"
+        self.assert_encrypt_then_decrypt(data, key)
 
+    def test_rand_small(self):
+        #test this with 10 random strings
+        for i in range(10):
+            rand_key_10 = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in range(10))
+            rand_data_10 = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in range(10))
+            self.assert_encrypt_then_decrypt(rand_data_10, rand_key_10);
 
