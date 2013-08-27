170ad0a414bbf559c5126025bf4c763d8d486145
Sunil Sadasivan <sunils34@gmail.com>
2013-08-27 14:56:57 -0700

    Add requires

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
 
 from distutils.core import setup
 
+requires_list = [
+    'hashlib==20081119']
+
 setup(
     name='xorcrypt',
     description='A simple python XOR encryption/decryption/obfuscation library',
-    version='0.1dev',
+    version='0.1',
     packages=['xorcrypt',],
     license='MIT License',
     long_description=open('README.md').read(),
@@ -12,5 +15,6 @@ setup(
     author_email="sunil@bufferapp.com.com",
     maintainer="Sunil Sadasivan",
     maintainer_email="sunil@bufferapp.com",
+    install_requires=requires_list,
     url="http://github.com/sunils34/python-xorcrypt"
 )
